01067d1a09a22534a52a676e2f2538fa5c284f51
Matthieu Etchegoyen <etchegom@gmail.com>
2019-10-14 16:21:29 +0200

    add options to search and fetch function

--- omdb_fixtures_loader/loader.py
+++ omdb_fixtures_loader/loader.py
@@ -9,22 +9,32 @@ class LoaderException(Exception):
     pass
 
 
-def _format_hit(hit: dict) -> dict:
+SOURCE_OPTION = "source"
+
+
+def _format_hit(hit: dict, options: dict = {}) -> dict:
     ret_hit = dict((k.lower(), v) for k, v in hit.items())
+
     for field in ("actors", "genre"):
+        if field not in ret_hit:
+            continue
         ret_hit[field] = [x.strip() for x in ret_hit[field].split(",")]
-    return ret_hit
+
+    if SOURCE_OPTION in options:
+        source_fields = options.get(SOURCE_OPTION, list())
+        return dict((k, v) for k, v in ret_hit.items() if k in source_fields)
+
+    return dict((k, v) for k, v in ret_hit.items())
 
 
-def search_and_fetch(api_key: str, search: str, media_type: str = "movie"):
+def search_and_fetch(api_key: str, search: str, media_type: str = "movie", **options):
     """Perform a search against the OMDB database and yield the returned hits
 
     Arguments:
         api_key {str} -- API key
         search {str} -- The searched text
-
-    Keyword Arguments:
         media_type {str} -- Type of media: "movie", "series" or "episode"  (default: {"movie"})
+        options {dict} -- extra options (ex: {"source": ["title", "actors"]})
     """
 
     resp = requests.get(
@@ -43,4 +53,4 @@ def search_and_fetch(api_key: str, search: str, media_type: str = "movie"):
         resp = requests.get(
             base_url, params={"apikey": api_key, "i": hit.get("imdbID")}
         )
-        yield _format_hit(resp.json())
+        yield _format_hit(resp.json(), options)
